8fdc851d0d038b01d83116a5bb1e0cd4b5f1cfe6
Cuttlefish <37397537+ddgksf2013@users.noreply.github.com>
2022-02-17 17:04:47 +0800

    Update elabook.js

--- Crack/elabook.js
+++ Crack/elabook.js
@@ -1,17 +1,17 @@
 /*
 
-> 应用名称：咿啦看书
-> 软件版本：2.8.5
-> 下载地址：https://apps.apple.com/cn/app/id1176775033
-> 脚本作者：Cuttlefish
-> 微信账号：墨鱼手记
-> 更新时间：2022-02-17
-> 通知频道：https://t.me/ddgksf2021
-> 问题反馈：https://t.me/ddgksf2013_bot
+> 應用名稱：咿啦看書
+> 軟件版本：2.8.5
+> 下載地址：https://apps.apple.com/cn/app/id1176775033
+> 腳本作者：Cuttlefish
+> 微信賬號：墨魚手記
+> 更新時間：2022-02-17
+> 通知頻道：https://t.me/ddgksf2021
+> 問題反饋：https://t.me/ddgksf2013_bot
 
 [rewrite_local]
 
-# ～ 咿啦看书解锁会员权限（2022-02-17）@ddgksf2013
+# ～ 咿啦看書解鎖會員權限（2022-02-17）@ddgksf2013
 https?:\/\/bookapi\.ellabook\.cn\/rest\/api\/service$ url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/elabook.js
 
 [mitm] 
