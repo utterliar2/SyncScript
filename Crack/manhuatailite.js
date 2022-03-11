6a235e6b146784f8cea819e932c938aff87bdabd
Cuttlefish <37397537+ddgksf2013@users.noreply.github.com>
2022-03-11 20:13:15 +0800

    Update manhuatailite.js

--- Crack/manhuatailite.js
+++ Crack/manhuatailite.js
@@ -21,4 +21,4 @@ hostname=comic.321mh.com
 
 
 var cuttlefish ={"warning":"本腳本僅供學習交流使用，禁止轉載售賣","tgchannel":"https://t.me/ddgksf2021","feedback":"https://t.me/ddgksf2013_bot"}
-$done({body.replace(/price":\d+/g,'price":0');});
+$done({body.replace(/price":\d+/g,'price":0')});
