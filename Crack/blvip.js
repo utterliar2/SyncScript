46c1f014d746a307b98085521612d4d94e79bdf7
Cuttlefish <37397537+ddgksf2013@users.noreply.github.com>
2022-03-28 18:58:51 +0800

    Create blvip.js

---
+++ Crack/blvip.js
@@ -0,0 +1,23 @@
+/***********************************
+
+> 應用名稱：B站大会员
+> 軟件版本：6.63.0
+> 腳本作者：Cuttlefish
+> 微信賬號：墨魚手記
+> 更新時間：2022-03-28
+> 通知頻道：https://t.me/ddgksf2021
+> 問題反饋：https://t.me/ddgksf2013_bot
+> 特别說明：本腳本僅供學習交流使用，禁止轉載售賣
+ 
+[rewrite_local]
+
+# ～ B站大会员（2022-03-17）@ddgksf2013
+^http[s]?:\/\/((app|api)\.(\w{2,15})?\.(com|cn|net|org)).*(playurl|player|reply)?\.(v3|v2|v1)\.(View|Reply|Play(URL|View|Conf)).*$ url script-response-body https://github.com/ddgksf2013/Cuttlefish/blob/master/Crack/blvip.js
+
+[mitm] 
+
+hostname=app.bilibili.com, grpc.biliapi.net,*.biliapi.net,app.bilibili.com,api.bilibili.com,api.live.bilibili.com,api.vc.bilibili.com,dataflow.biliapi.com
+
+***********************************/
+
+var _0xod1='jsjiami.com.v6',_0xod1_=['_0xod1'],_0x1f9b=[_0xod1,'\u672c\u8173\u672c\u50c5\u4f9b\u5b78\u7fd2\u4ea4\u6d41\u4f7f\u7528\uff0c\u7981\u6b62\u8f49\u8f09\u552e\u8ce3','\x68\x74\x74\x70\x73\x3a\x2f\x2f\x74\x2e\x6d\x65\x2f\x64\x64\x67\x6b\x73\x66\x32\x30\x32\x31','\x68\x74\x74\x70\x73\x3a\x2f\x2f\x74\x2e\x6d\x65\x2f\x64\x64\x67\x6b\x73\x66\x32\x30\x31\x33\x5f\x62\x6f\x74','\x63\x6f\x6d\x2e\x72\x65\x61\x64\x64\x6c\x65\x2e\x52\x65\x61\x64\x64\x6c\x65\x44\x6f\x63\x73\x49\x50\x61\x64\x2e\x73\x75\x62\x73\x63\x72\x69\x70\x74\x69\x6f\x6e\x2e\x79\x65\x61\x72\x35\x30\x5f\x75\x70\x64\x5f\x75\x73\x65\x72','\x75\x73\x65\x72\x43\x61\x6e\x63\x65\x6c\x6c\x65\x64','\x31\x37\x3a\x34\x38\x20\x32\x35\x2f\x31\x31\x2f\x32\x30\x39\x39','\x31\x37\x3a\x34\x38\x20\x32\x35\x2f\x31\x31\x2f\x32\x30\x31\x39','\x32\x30\x30\x30\x30\x36\x32\x35\x34\x32\x30\x31\x30\x32','\x73\x75\x62\x73\x63\x72\x69\x70\x74\x69\x6f\x6e','\x61\x63\x74\x69\x76\x65','\x61\x75\x74\x6f\x52\x65\x6e\x65\x77\x4f\x66\x66','\x30\x30\x30\x30','\x63\x75\x73\x74\x6f\x6d\x20\x70\x75\x72\x63\x68\x61\x73\x65','\x64\x6f\x63\x75\x6d\x65\x6e\x74\x73\x36\x2d\x75\x73\x65\x72','\x77\x69\x6e\x62\x61\x63\x6b','\x63\x6f\x6d\x2e\x72\x65\x61\x64\x64\x6c\x65\x2e\x52\x65\x61\x64\x64\x6c\x65\x44\x6f\x63\x73\x49\x50\x61\x64','\x69\x4f\x53\x20\x41\x70\x70\x53\x74\x6f\x72\x65','\x6a\x5a\x73\x48\x6a\x69\x61\x75\x67\x70\x4e\x6d\x68\x69\x54\x55\x2e\x79\x63\x70\x53\x65\x6f\x56\x6d\x2e\x76\x36\x47\x3d\x3d'];function _0x50c0(_0x1c3a4d,_0x26bfae){_0x1c3a4d=~~'0x'['concat'](_0x1c3a4d['slice'](0x0));var _0xb6e6d5=_0x1f9b[_0x1c3a4d];return _0xb6e6d5;};(function(_0x44943f,_0xcb0bc1){var _0x22900e=0x0;for(_0xcb0bc1=_0x44943f['shift'](_0x22900e>>0x2);_0xcb0bc1&&_0xcb0bc1!==(_0x44943f['pop'](_0x22900e>>0x3)+'')['replace'](/[ZHugpNhTUypSeVG=]/g,'');_0x22900e++){_0x22900e=_0x22900e^0xda810;}}(_0x1f9b,_0x50c0));var cuttlefish={'warning':_0x50c0('0'),'tgchannel':_0x50c0('1'),'feedback':_0x50c0('2')};var ddgksf2013={'productId':_0x50c0('3'),'subscriptionExpirationIntent':_0x50c0('4'),'receiptStatus':'\x6f\x6b','subscriptionExpirationDate':_0x50c0('5'),'inAppStates':[{'productId':_0x50c0('3'),'subscriptionExpirationIntent':_0x50c0('4'),'receiptStatus':'\x6f\x6b','subscriptionExpirationDate':_0x50c0('6'),'isEligibleForIntroPeriod':![],'originalTransactionId':_0x50c0('7'),'productName':_0x50c0('8'),'isInBillingRetryPeriod':![],'type':_0x50c0('8'),'subscriptionState':_0x50c0('9'),'subscriptionAutoRenewStatus':_0x50c0('a'),'isInGracePeriod':![]},{'originalTransactionId':_0x50c0('b'),'entitlements':[],'type':_0x50c0('c'),'productId':_0x50c0('d')}],'isDocuments6User':!![],'isEligibleForIntroPeriod':![],'originalTransactionId':_0x50c0('7'),'isEligibleFor':[_0x50c0('e')],'isInBillingRetryPeriod':![],'type':_0x50c0('8'),'inAppPurchased':[_0x50c0('3')],'bundleId':_0x50c0('f'),'receiptId':0x1647142f960,'chargingPlatform':_0x50c0('10'),'subscriptionState':_0x50c0('9'),'subscriptionAutoRenewStatus':_0x50c0('a'),'isInGracePeriod':![]};$done({'\x62\x6f\x64\x79':body});;_0xod1='jsjiami.com.v6';
