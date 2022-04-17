b885099d287785f1f7984a7fad0865c42b95be21
Cuttlefish <37397537+ddgksf2013@users.noreply.github.com>
2022-04-17 21:53:55 +0800

    Update 60s.js

--- Script/60s.js
+++ Script/60s.js
@@ -1,11 +1,15 @@
 /*
-
-通知频道：https://t.me/ddgksf2021
-微信账号：墨鱼手记
-更新时间：2022-04-17 
+* ==UserScript==
+* @ScriptName        每天60s读懂世界
+* @Author            Cuttlefish
+* @TgChannel         https://t.me/ddgksf2021
+* @WechatID          墨鱼手记
+* @UpdateTime        2022-04-17
+* @ScriptFunction    快速浏览今天世界新闻
+* @ScriptURL         https://github.com/ddgksf2013/Cuttlefish/raw/master/Script/60s.js
+* ==/UserScript==
 
 [task_local]
-
 0 9 * * * https://github.com/ddgksf2013/Cuttlefish/raw/master/Script/60s.js, tag=每天60s读懂世界
 
 */
