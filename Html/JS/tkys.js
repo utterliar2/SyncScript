ccde9d22cc433cad5485130580db494e37e19e8b
Cuttlefish <37397537+ddgksf2013@users.noreply.github.com>
2022-05-29 09:28:12 +0800

    Update tkys.js

--- Html/JS/tkys.js
+++ Html/JS/tkys.js
@@ -1,3 +1,3 @@
 var body = $response.body
-    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Cuttlefish@master/Html/CSS/tkys.css" type="text/css"');
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Cuttlefish@master/Html/CSS/tkys.css" type="text/css">');
 $done({ body });
