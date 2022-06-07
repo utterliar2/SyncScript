f974964803113a353fc8539085907cdd7a2c58d3
Cuttlefish <37397537+ddgksf2013@users.noreply.github.com>
2022-06-07 15:22:36 +0800

    Update cupbox.js

--- Html/JS/cupbox.js
+++ Html/JS/cupbox.js
@@ -1,3 +1,3 @@
 var body = $response.body
-    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Cuttlefish@v0.3/Html/CSS/cupbox.css" type="text/css">');
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Cuttlefish/Html/CSS/cupbox.css" type="text/css">');
 $done({ body });
