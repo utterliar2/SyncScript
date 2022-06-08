7cc51fc19fd8e14f09287cc00d3833417377a067
Cuttlefish <37397537+ddgksf2013@users.noreply.github.com>
2022-06-08 11:50:52 +0800

    Update wechat_auto_read.js

--- Script/wechat_auto_read.js
+++ Script/wechat_auto_read.js
@@ -38,5 +38,5 @@ hostname = mp.weixin.qq.com
 */
 
 var body = $response.body
-    .replace(/</script>/, 'setTimeout(()=>window.history.back(),6000); </script>');
+    .replace(/<\/script>/, 'setTimeout(()=>window.history.back(),6000); </script>');
 $done({ body });
