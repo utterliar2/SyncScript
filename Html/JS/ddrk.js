2eb68ff2bbcc13f7473ee6f5c70a7ee35625bd23
Cuttlefish <37397537+ddgksf2013@users.noreply.github.com>
2022-08-09 20:14:29 +0800

    Update ddrk.js

--- Html/JS/ddrk.js
+++ Html/JS/ddrk.js
@@ -1,3 +1,3 @@
 var body = $response.body
-    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Cuttlefish@v0.3/Html/CSS/ddrk.css" type="text/css"><script type="text/javascript" async="async" src="//limbopro.com/Adguard/ddrk.js"></script>');
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Cuttlefish@v0.3/Html/CSS/ddrk.css" type="text/css">');
 $done({ body });
