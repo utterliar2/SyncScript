2012b862c25964e618ddb370d788c14113904f10
Cuttlefish <37397537+ddgksf2013@users.noreply.github.com>
2022-08-12 15:15:47 +0800

    Update sample.js

--- Crack/sample.js
+++ Crack/sample.js
@@ -16,5 +16,6 @@
 // @/g                正则匹配时匹配多个key键值
 // ==/KeyExplain==
 
+
 var body = $response.body.replace(/"key1":originalValue/g, '"key1":replaceValue').replace(/"key2":originalValue/g, '"key2":replaceValue')
 $done({ body });
